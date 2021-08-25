cb09dfa279d53e92e522af3906432c952dc52f9b
NonThanadol <81633477+nonthanadol@users.noreply.github.com>
2021-08-25 04:23:50 +0000

    Add files via upload

---
+++ key_grip.py
@@ -0,0 +1,33 @@
+import sys, tty, termios, os
+from GripperControl import AlphaBot
+
+bot=AlphaBot()
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+bot.setPWMA(70)
+while True:
+  try: 
+   char = getch()
+   if(char == 'q'):
+      print('q')
+      bot.forward()
+   elif(char == 'a'):
+      print('a')
+      bot.backward()
+   elif(char == 'z'):
+      print('z')
+      bot.stop()
+   elif(char == "x"):
+      break
+   char = ""
+  except ValueError:
+    print('Angle out of range')                        
